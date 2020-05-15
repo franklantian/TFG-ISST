07a90e23db2688f849d671080da8196649b5e154
Tian Lan <t.lan@alumnos.upm.es>
2020-05-15 20:01:39 +0200

    TFG DAO Implementation 已建立，需要完成TFG的login的Prueba

--- ISST-20-TFG-0/src/es/upm/dit/isst/tfg/dao/TFGDAOImplementation.java
+++ ISST-20-TFG-0/src/es/upm/dit/isst/tfg/dao/TFGDAOImplementation.java
@@ -1,11 +1,17 @@
 package es.upm.dit.isst.tfg.dao;
 
 import java.util.Collection;
+import java.util.List;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+
+import es.upm.dit.isst.tfg.model.Professor;
 import es.upm.dit.isst.tfg.model.TFG;
 
 public class TFGDAOImplementation implements TFGDAO {
-	
+
 	private static TFGDAOImplementation instancia = null;
 
 	private TFGDAOImplementation() {
@@ -18,40 +24,96 @@ public class TFGDAOImplementation implements TFGDAO {
 		return instancia;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void create(TFG tfg) {
-		// TODO Auto-generated method stub
+		Session session = SessionFactoryService.get().openSession(); 
+		session.beginTransaction();
+
+		session.save(tfg); 
+
+		session.getTransaction().commit();
+		session.close();
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public TFG read(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = SessionFactoryService.get().openSession(); 
+		session.beginTransaction();
+
+		TFG tfg = session.get(TFG.class, email); 
+
+		session.getTransaction().commit();
+		session.close();
+
+		return tfg;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void update(TFG tfg) {
-		// TODO Auto-generated method stub
+		Session session = SessionFactoryService.get().openSession(); 
+		session.beginTransaction();
+
+		session.saveOrUpdate(tfg); 
+
+		session.getTransaction().commit();
+		session.close();
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void delete(TFG tfg) {
-		// TODO Auto-generated method stub
+		Session session = SessionFactoryService.get().openSession(); 
+		session.beginTransaction();
+
+		session.delete(tfg); 
+
+		session.getTransaction().commit();
+		session.close();
 
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<TFG> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = SessionFactoryService.get().openSession(); 
+		session.beginTransaction();
+
+		List<TFG> tfgs = session.createQuery("from TFG").list();
+
+		session.getTransaction().commit();
+		session.close();
+		return tfgs;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public TFG login(String email, String psd) {
+	public TFG login(String email, String password) {
 		// TODO Auto-generated method stub
-		return null;
+		//abrir una session
+		Session session = SessionFactoryService.get().openSession(); 
+		TFG t = null;
+		//arrangar transaccion
+		session.beginTransaction();
+
+		//Operacion real
+		Query q = session.createQuery("select t from Professor t where t.email = :email and t.password = :password");
+		q.setParameter("email", email); 
+		q.setParameter("password", password); 
+		List<TFG> tfgs = q.getResultList(); 
+		if (tfgs.size() > 0)
+			t = (TFG) (q.getResultList().get(0));
+		//Prueba de modelos
+
+
+		//cerrar la transaccion y session
+		session.getTransaction().commit();
+		session.close();
+		return t;
 	}
 
 }
